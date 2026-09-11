6180ba2eb40fd5792c732b525009085995fb43b9
YungOfficial <rokeebabiodun32@gmail.com>
2026-09-11 06:00:55 +0100

    Add intro.js with personal information logging

---
+++ intro.js
@@ -0,0 +1,8 @@
+// intro.js
+let name = "Your Name";
+let age = 20;
+let favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
